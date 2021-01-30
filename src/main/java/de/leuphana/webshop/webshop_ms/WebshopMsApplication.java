c3f4f330e8f5116946ba1f0d460a17a78c80bb9b
Karan Bruellau <karan@Karans-MBP-2.fritz.box>
2021-01-30 20:26:42 +0100

    Initial commit

---
+++ src/main/java/de/leuphana/webshop/webshop_ms/WebshopMsApplication.java
@@ -0,0 +1,13 @@
+package de.leuphana.webshop.webshop_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopMsApplication.class, args);
+	}
+
+}
