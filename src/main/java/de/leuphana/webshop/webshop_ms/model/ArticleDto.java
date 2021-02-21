a5e506a0fd4277ca08bf0adc7a28433553f0c650
karan <karan.bruellau1997@gmail.com>
2021-02-21 14:43:47 +0100

    added validation for ArticleDto

--- src/main/java/de/leuphana/webshop/webshop_ms/model/ArticleDto.java
+++ src/main/java/de/leuphana/webshop/webshop_ms/model/ArticleDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -15,12 +19,22 @@ import java.util.UUID;
 @Builder
 public class ArticleDto {
 
+    @Null
     private UUID articleId;
+    @Null
     private String manufacturer;
-    private String name;
-    private BigDecimal price;
+    @Null
     private OffsetDateTime createdDate;
+    @Null
     private OffsetDateTime lastModified;
+
+    @NotBlank
+    private String name;
+
+    @Positive
+    @NotNull
+    private BigDecimal price;
+
     private Integer quantityOnHand;
 
 }
