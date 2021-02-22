f5264dc3700c96710625e995b36ae9392319b855
karan <karan.bruellau1997@gmail.com>
2021-02-22 17:42:46 +0100

    temporary comments for bugfix

--- src/test/java/de/leuphana/webshop/webshop_ms/controller/ArticleDtoControllerTest.java
+++ src/test/java/de/leuphana/webshop/webshop_ms/controller/ArticleDtoControllerTest.java
@@ -2,6 +2,8 @@ package de.leuphana.webshop.webshop_ms.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.leuphana.webshop.webshop_ms.model.ArticleDto;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -10,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -25,7 +28,19 @@ class ArticleDtoControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    ArticleDto validArticle;
+
     // These tests test if Mapping works correctly
+
+//    @Before
+//    public void setUp() {
+//        validArticle = ArticleDto.builder()
+//                .articleId(UUID.randomUUID())
+//                .name("Book")
+//                .manufacturer("Springer")
+//                .price(new BigDecimal("10"))
+//                .build();
+//    }
 
     @Test
     void getArticleById() throws Exception {
@@ -43,6 +58,7 @@ class ArticleDtoControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(articleJson))
                 .andExpect(status().isCreated());
+
     }
 
     @Test
